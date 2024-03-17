b16408012b2e765c079d59256129773c4f7a7975
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-03-16 21:34:59 -0700

    Add clerk for future user authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
 import App from './App.jsx';
 import 'material-icons/iconfont/filled.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <App />
+  </ClerkProvider>
+);
