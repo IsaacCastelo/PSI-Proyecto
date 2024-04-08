c6dc71641cbb804f8ef42dc2ad24d0abb24f297e
terquimocrio <diego_25mj@hotmail.com>
2024-04-07 21:02:24 -0700

    regresa id tipo pedido y nombre cliente

--- apps/punto_de_venta/serializers.py
+++ apps/punto_de_venta/serializers.py
@@ -14,7 +14,7 @@ class PlatilloSerializer(serializers.ModelSerializer):
 class PedidoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pedido
-        fields = ('mesa', 'tipo_pedido')
+        fields = ('mesa', 'tipo_pedido', 'id', 'nombre_cliente')
 
 class DetallePedidoSerializer(serializers.ModelSerializer):
     class Meta:
