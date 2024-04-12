338794aba321a33a7d20e2511d954bba9f5236c8
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-04-12 16:16:29 -0700

    Remueve clerk, arregla el botón a la hora de cambiar estado de órden

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
-import { ClerkProvider } from '@clerk/clerk-react';
+//import { ClerkProvider } from '@clerk/clerk-react';
 import App from './components/App/App';
 import 'material-icons/iconfont/filled.css';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+//const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  //<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <>
     <Toaster />
     <App />
-  </ClerkProvider>
+  </>
+
+  // </ClerkProvider>
 );
