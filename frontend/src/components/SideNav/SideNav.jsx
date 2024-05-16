a60178057f6f3912468d569c75f040f25544c5e1
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-05-16 03:16:46 -0700

    Add login and logout flows

--- frontend/src/components/SideNav/SideNav.jsx
+++ frontend/src/components/SideNav/SideNav.jsx
@@ -1,68 +1,62 @@
 import logo from '../../images/logo.png';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useAuth } from '../../hooks/AuthContext';
 
-SideNav.propTypes = {
-  isLogged: PropTypes.bool,
-  setIsLogged: PropTypes.func,
-};
-
-export default function SideNav({ isLogged, setIsLogged }) {
+export default function SideNav() {
   const [lastClicked, setLastClicked] = useState('dashboard');
+  const { logout } = useAuth();
 
   const handleClick = (element) => {
     setLastClicked(element);
   };
 
   function handleLogout() {
-    setIsLogged(false);
+    logout();
   }
 
   return (
-    isLogged && (
-      <nav className='flex flex-col w-72 h-screen bg-gray-700 text-white px-5'>
-        <img src={logo} alt='logo' />
-        <ul className='flex py-1 flex-col gap-4'>
-          <li
-            className={`flex nav-link justify-between p-1 rounded ${
-              lastClicked === 'dashboard' ? 'bg-gray-500' : ''
-            }`}
-          >
-            <Link to='dashboard' onClick={() => handleClick('dashboard')}>
-              Administrar ordenes
-            </Link>
-            <span className='material-icons'>library_books</span>
-          </li>
-          <li
-            className={`flex nav-link justify-between p-1 rounded ${
-              lastClicked === 'products' ? 'bg-gray-500' : ''
-            }`}
-          >
-            <Link to='products' onClick={() => handleClick('products')}>
-              Administrar productos
-            </Link>
-            <span className='material-icons'>manage_search</span>
-          </li>
-          <li
-            className={`flex nav-link justify-between p-1 rounded ${
-              lastClicked === 'employees' ? 'bg-gray-500' : ''
-            }`}
-          >
-            <Link to='employees' onClick={() => handleClick('employees')}>
-              Administrar empleados
-            </Link>
-            <span className='material-icons'>manage_accounts</span>
-          </li>
-        </ul>
-        <Link
-          to='login'
-          className='bg-red-500 rounded p-1 text-white hover:bg-red-700 transition-all mt-auto mb-3 text-center'
-          onClick={handleLogout}
+    <nav className='flex flex-col w-fit h-screen bg-gray-700 text-white px-5'>
+      <img src={logo} alt='logo' className='h-fit w-[328px]' />
+      <ul className='flex py-1 flex-col gap-4'>
+        <li
+          className={`flex nav-link justify-between p-1 rounded ${
+            lastClicked === 'dashboard' ? 'bg-gray-500' : ''
+          }`}
         >
-          Cerrar sesión
-        </Link>
-      </nav>
-    )
+          <Link to='dashboard' onClick={() => handleClick('dashboard')}>
+            Administrar ordenes
+          </Link>
+          <span className='material-icons'>library_books</span>
+        </li>
+        <li
+          className={`flex nav-link justify-between p-1 rounded ${
+            lastClicked === 'products' ? 'bg-gray-500' : ''
+          }`}
+        >
+          <Link to='products' onClick={() => handleClick('products')}>
+            Administrar productos
+          </Link>
+          <span className='material-icons'>manage_search</span>
+        </li>
+        <li
+          className={`flex nav-link justify-between p-1 rounded ${
+            lastClicked === 'employees' ? 'bg-gray-500' : ''
+          }`}
+        >
+          <Link to='employees' onClick={() => handleClick('employees')}>
+            Administrar empleados
+          </Link>
+          <span className='material-icons'>manage_accounts</span>
+        </li>
+      </ul>
+      <Link
+        to='login'
+        className='bg-red-500 rounded p-1 text-white hover:bg-red-700 transition-all mt-auto mb-3 text-center'
+        onClick={handleLogout}
+      >
+        Cerrar sesión
+      </Link>
+    </nav>
   );
 }
