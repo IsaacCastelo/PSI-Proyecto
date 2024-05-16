36f1c13107ea93cd0417bd131306ae0390a33310
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-05-16 03:16:46 -0700

    Add login and logout flows

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,16 +1,17 @@
-import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useAuth } from '../../hooks/AuthContext';
+import { login } from '../../api/api';
 
-Login.propTypes = {
-  setIsLogged: PropTypes.func.isRequired,
-};
+export default function Login() {
+  const auth = useAuth();
+  const { register, handleSubmit } = useForm();
 
-export default function Login({ setIsLogged }) {
-  const navigate = useNavigate();
-
-  function handleLogin() {
-    setIsLogged(true);
-    navigate('/dashboard');
+  function handleLogin(data) {
+    login(data).then((response) => {
+      if (response) {
+        auth.login(response.access);
+      }
+    });
   }
 
   return (
@@ -19,17 +20,25 @@ export default function Login({ setIsLogged }) {
         <h1 className='text-2xl'>Iniciar sesión</h1>
         <p className='text-sm text-gray-500'>Ingresa tus datos</p>
       </header>
-      <form className='flex flex-col items-center gap-3'>
-        <h2>Datos del empleado</h2>
-        <input placeholder='Nombre de usuario' className='rounded shadow p-1' />
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className='flex flex-col items-center gap-3'
+      >
+        <h2>Datos de usuario</h2>
         <input
+          {...register('nombre_usuario', { required: true })}
+          placeholder='Nombre de usuario'
+          className='rounded shadow p-1'
+        />
+        <input
+          {...register('contraseña', { required: true })}
           placeholder='Contraseña'
           type='password'
           className='rounded shadow px-1'
         />
         <button
           className='bg-blue-600 rounded px-14 text-white font-bold py-1 hover:bg-blue-800 transition-all hover:text-gray-400'
-          onClick={handleLogin}
+          type='submit'
         >
           Iniciar sesión
         </button>
