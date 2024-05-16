ae3341414ad4a8135880d70c4f583be9ba01e1e9
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-05-16 03:09:20 -0700

    Update serializers

--- apps/punto_de_venta/serializers.py
+++ apps/punto_de_venta/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Platillo, Pedido, DetallePedido, IngresoDiario, Movimiento, Reporte
+from .models import Platillo, Pedido, DetallePedido, IngresoDiario, Movimiento, Reporte, Usuario
 
 # class MesaSerializer(serializers.ModelSerializer):
 #     class Meta:
@@ -36,7 +36,22 @@ class ReporteSerializer(serializers.ModelSerializer):
         model = Reporte
         fields = '__all__'
 
-# class UsuarioSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Usuario
-#         fields = '__all__'
+class UsuarioSerializer(serializers.ModelSerializer):
+    contraseña = serializers.CharField(write_only=True)  # Campo de contraseña solo para escritura
+    class Meta:
+        model = Usuario
+        fields = ('id', 'nombre_empleado', 'nombre_usuario', 'rol', 'contraseña')
+
+    def create(self, validated_data):
+        # Extraer la contraseña del validated_data
+        contraseña = validated_data.pop('contraseña', None)
+
+        # Crear el usuario sin la contraseña
+        usuario = Usuario.objects.create(**validated_data)
+
+        # Establecer la contraseña y guardar el usuario
+        if contraseña is not None:
+            usuario.contraseña = contraseña
+            usuario.save()
+
+        return usuario
