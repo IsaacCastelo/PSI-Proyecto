b5a47d21b9f1622ee366d31efbde96229e37c9b2
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-05-16 03:08:48 -0700

    Remove clerk

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
-//import { ClerkProvider } from '@clerk/clerk-react';
 import App from './components/App/App';
 import 'material-icons/iconfont/filled.css';
 import React from 'react';
 
-//const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-  <>
-    <React.StrictMode>
-      <Toaster />
-      <App />
-    </React.StrictMode>
-  </>
-
-  // </ClerkProvider>
+  <React.StrictMode>
+    <Toaster />
+    <App />
+  </React.StrictMode>
 );
