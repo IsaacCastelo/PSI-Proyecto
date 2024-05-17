8c07498a746a46ed3126a6808fd44d34e854c73e
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-05-17 14:32:32 -0700

    Remove react strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,10 @@ import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import App from './components/App/App';
 import 'material-icons/iconfont/filled.css';
-import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <Toaster />
     <App />
-  </React.StrictMode>
+  </>
 );
